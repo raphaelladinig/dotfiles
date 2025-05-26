474f1dc73bb42e18a7793e1af06ee14c7a6dd05c
Raphael Ladinig <mail@raphaelladinig.com>
2025-05-26 07:53:30 +0200

    fix: properly delete old_root

--- hosts/common/impermanence.nix
+++ hosts/common/impermanence.nix
@@ -19,16 +19,26 @@ in {
       before = ["sysroot.mount"];
       serviceConfig.Type = "oneshot";
       script = ''
-        mkdir /btrfs_tmp
-        mount /dev/root_vg/root /btrfs_tmp
-        if [[ -e /btrfs_tmp/root ]]; then
-          if [[ -e /btrfs_tmp/old_root ]]; then
-            btrfs subvolume delete /btrfs_tmp/old_root
-          fi
-          mv /btrfs_tmp/root /btrfs_tmp/old_root
-        fi
-        btrfs subvolume create /btrfs_tmp/root
-        umount /btrfs_tmp
+            mkdir /btrfs_tmp
+            mount /dev/root_vg/root /btrfs_tmp
+
+            delete_subvolume_recursively() {
+              IFS=$'\n'
+              for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                  delete_subvolume_recursively "/btrfs_tmp/$i"
+              done
+              btrfs subvolume delete "$1"
+            }
+            if [[ -e /btrfs_tmp/old_root ]]; then
+              delete_subvolume_recursively "/btrfs_tmp/old_root"
+            fi
+
+            if [[ -e /btrfs_tmp/root ]]; then
+              mv /btrfs_tmp/root /btrfs_tmp/old_root
+            fi
+
+            btrfs subvolume create /btrfs_tmp/root
+            umount /btrfs_tmp
       '';
     };
   };
